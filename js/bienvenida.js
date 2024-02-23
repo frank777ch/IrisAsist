c300b35e0909d208debf553b16a663a4b3f5429a
frank777ch <150204523+frank777ch@users.noreply.github.com>
2024-02-23 08:04:39 -0500

    Cambio principal

--- js/bienvenida.js
+++ js/bienvenida.js
@@ -5,4 +5,15 @@ function reproducirMensaje() {
     mensaje.lang = "es-E"; 
  
     window.speechSynthesis.speak(mensaje);
+
+    // Ejecutar la síntesis de voz
+    window.speechSynthesis.speak(mensaje);
+
+    // Agregar clase "active" a la imagen para aplicar el efecto
+    document.getElementById("logo").classList.add("active");
+
+    // Remover clase "active" después de un tiempo para volver a la normalidad
+    setTimeout(function() {
+        document.getElementById("logo").classList.remove("active");
+    }, 200); // El mismo tiempo que la transición en CSS
 }
