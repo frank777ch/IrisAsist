da7dbbdb3e222b7591c0c0fec8529a10dfbae588
frank777ch <150204523+frank777ch@users.noreply.github.com>
2024-02-23 01:56:32 -0500

    Second change

--- js/bienvenida.js
+++ js/bienvenida.js
@@ -1,4 +1,8 @@
-window.onload = function() {
-    alert("¡Bienvenido a mi página web!");
-  };
-  
+function reproducirMensaje() {
+
+    var mensaje = new SpeechSynthesisUtterance("¡Bienvenido a Iris Asist, como docente podrás realizar las siguientes opciones: Opción 1: Administrar alumnos. Opción 2: Administrar notas. Por favor escoja una de las dos opciones disponibles.");
+
+    mensaje.lang = "es-E"; 
+ 
+    window.speechSynthesis.speak(mensaje);
+}
